de7be7619610b6863d6f4e3edbdf488ef7f218c0
Zhongxing Peng <singlepetrol@gmail.com>
2016-12-12 22:23:42 -0600

    In setup.py, modify install_requires and __version__

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import os
 from setuptools import setup
 
 CURRENT_DIR = os.path.dirname(__file__)
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 
 setup(
     name='smile',
     version=__version__,
     description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
-    install_requires=['requests'],
+    install_requires=[],
     author='Zheng Xu',
     author_email='zheng.xu@mavs.uta.edu',
     zip_safe=False,
