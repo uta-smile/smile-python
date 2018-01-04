5777f0b55c2287e56a2c7a7617bbcf8f33c9749a
Zheng Xu <xuzheng1111@gmail.com>
2018-01-03 23:03:16 -0600

    Update smoke_test with subcommand.

--- tests/smoke_tests/smoke_test.py
+++ tests/smoke_tests/smoke_test.py
@@ -10,7 +10,13 @@ import smile as sm
 from smile import flags
 from smile import logging
 
-flags.DEFINE_string("param", "default_value", "some help infomation")
+flags.DEFINE_string("param", "default_value", "A general flag.")
+
+with flags.Subcommand("echo", dest="action"):
+    flags.DEFINE_string("echo_text", "", "The text to be echoed out.")
+
+with flags.Subcommand("echo_bool", dest="action"):
+    flags.DEFINE_bool("just_do_it", False, "some help infomation")
 
 FLAGS = flags.FLAGS
 
@@ -18,6 +24,10 @@ FLAGS = flags.FLAGS
 def main(_):
     """Print out the FLAGS in the main function."""
     logging.info("param = %s", FLAGS.param)
+    if FLAGS.action == "echo":
+        logging.warn(FLAGS.echo_text)
+    elif FLAGS.action == "echo_bool":
+        logging.info("Just do it? %s", "Yes!" if FLAGS.just_do_it else "No :(")
 
 
 if __name__ == "__main__":
--- tests/smoke_tests/smoke_test.sh
+++ tests/smoke_tests/smoke_test.sh
@@ -6,7 +6,11 @@ echo "Starting smoke tests..."
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 SMOKE_TEST_PY="${DIR}/smoke_test.py"
 
-RET=`python $SMOKE_TEST_PY 2>&1`
+RET=`python $SMOKE_TEST_PY echo --echo_text "hello world!" 2>&1`
 echo $RET | grep 'param = default_value'
+echo $RET | grep 'hello world!'
+
+python $SMOKE_TEST_PY echo_bool 2>&1 | grep "Just do it? No :("
+python $SMOKE_TEST_PY echo_bool --just_do_it 2>&1 | grep "Just do it? Yes!"
 
 echo "Smoke tests passed."
