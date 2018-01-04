2cbc32bb350139a61341fa5361e45745dd3b82e6
Zheng Xu <xuzheng1111@gmail.com>
2018-01-03 23:20:50 -0600

    Add stronger smoke test.

--- tests/smoke_tests/smoke_test.sh
+++ tests/smoke_tests/smoke_test.sh
@@ -10,6 +10,10 @@ RET=`python $SMOKE_TEST_PY echo --echo_text "hello world!" 2>&1`
 echo $RET | grep 'param = default_value'
 echo $RET | grep 'hello world!'
 
+RET=`python $SMOKE_TEST_PY --param "new_value" echo_bool --just_do_it 2>&1`
+echo $RET | grep 'param = new_value'
+echo $RET | grep 'Just do it? Yes!'
+
 python $SMOKE_TEST_PY echo_bool 2>&1 | grep "Just do it? No :("
 python $SMOKE_TEST_PY echo_bool --just_do_it 2>&1 | grep "Just do it? Yes!"
 
